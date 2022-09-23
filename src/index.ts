27421d0b2e2ebddc024c10abd2f961ce923412e6
Angel Dominguez <angeldomp49@gmail.com>
2022-09-23 09:48:39 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { Theme } from "@makechtec/tlapali_dev";
+
+export const Makech: Theme = {
+    name: "Makech",
+    styleSets: [
+        {
+            rule: "class_declaration",
+            "styles": [
+                "color: red;"
+            ]
+        }
+    ]
+}
