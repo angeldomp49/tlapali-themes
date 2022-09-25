4db8e058c6336cce71247b26e932b19f76b43e0e
Angel Dominguez <angeldomp49@gmail.com>
2022-09-24 20:10:33 -0500

    themes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 import { Theme } from "@makechtec/tlapali_dev";
 
+const classStyles = [
+    "color: #012A68;",
+    "font-family: roboto;",
+    "font-weight: bold;"
+];
+
+const identifierStyles = [
+    "color: #2A058B;",
+    "font-family: roboto;",
+    "font-weight: normal;"
+];
+
 export const Makech: Theme = {
     name: "Makech",
     supportedLaguages: [
@@ -9,9 +21,27 @@ export const Makech: Theme = {
                 {
                     rule: "class_declaration",
                     styles: [
-                        "color: red;"
+                        ...classStyles
                     ]
-                }
+                },
+                {
+                    rule: "import_declaration",
+                    styles: [
+                        ...classStyles
+                    ]
+                },
+                {
+                    rule: "variable_declaration",
+                    styles: [
+                        ...identifierStyles
+                    ]
+                },
+                {
+                    rule: "method_declaration",
+                    styles: [
+                        ...identifierStyles
+                    ]
+                },
             ]
         }
     ]
